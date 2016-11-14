7e2599e4afb632e9479a70045fb6a5c05b34918a
Raphael Catolino <raphael.catolino@gmail.com>
2016-11-14 15:50:50 +0100

    use scapy to parse packet

--- listen.py
+++ listen.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from scapy.all import IP, TCP, hexdump
 import socket
 import sys
 
@@ -7,14 +8,18 @@ lport = 8255
 lhost = '127.0.0.1'
 max_syn_size = 128
 
+"""
 def print_hex(data):
     for b in data:
         sys.stdout.write("{:02x}".format(b))
     sys.stdout.write("\n")
+"""
 
 s = socket.socket(family=socket.AF_INET, type=socket.SOCK_RAW, proto=socket.IPPROTO_TCP)
 s.bind((lhost, 0))
 
 while True:
-    packet = s.recv(max_syn_size)
-    print_hex(packet)
+    packet = IP(s.recv(max_syn_size))
+    for p in packet:
+        if p.dport == lport:
+            packet.show()
