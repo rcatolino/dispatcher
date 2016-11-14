10fe20108b715044906a69d8ad282849be1e0255
Raphael Catolino <raphael.catolino@gmail.com>
2016-11-14 19:04:44 +0100

    update stuff

--- listen.py
+++ listen.py
@@ -1,25 +1,57 @@
 #!/usr/bin/python3
 
+import random
 from scapy.all import IP, TCP, hexdump
 import socket
+from subprocess import run
 import sys
 
 lport = 8255
-lhost = '127.0.0.1'
+lhost = ''
 max_syn_size = 128
+portrange = list(random.sample(range(2000, 3000), 255))
 
-"""
-def print_hex(data):
-    for b in data:
-        sys.stdout.write("{:02x}".format(b))
-    sys.stdout.write("\n")
-"""
+FIN = 0x01
+SYN = 0x02
+RST = 0x04
+PSH = 0x08
+ACK = 0x10
+URG = 0x20
+ECE = 0x40
+CWR = 0x80
 
 s = socket.socket(family=socket.AF_INET, type=socket.SOCK_RAW, proto=socket.IPPROTO_TCP)
 s.bind((lhost, 0))
 
+mappings = {}
+
+def giveup(msg):
+    print("{}".format(msg))
+    exit(1)
+
+def remove_mapping(client):
+    print("Removing mapping for client {} to port {}".format(client, mapping[client][0]))
+    del mapping[client]
+
+def add_mapping(p):
+    if len(portrange) == 0:
+        # All slots are used, just kill everything and die
+        giveup("Error, no more mapping to allocate")
+    newport = portrange.pop()
+    print("Adding dynamic mapping from client {} to port {}".format(p.src, newport))
+    if mapping[p.src]:
+        remove_mapping(p.src)
+    container_id = docker_start(newport)
+    iptables_route(p.src, newport)
+    mapping[p.src] = (newport, container_id)
+
+def filter_packet(p):
+    return p.dport == lport and (lhost == '' or p.dst == lhost)
+
 while True:
-    packet = IP(s.recv(max_syn_size))
-    for p in packet:
-        if p.dport == lport:
-            packet.show()
+    packets = IP(s.recv(max_syn_size))
+    for p in packets:
+        if filter_packet(p) and (p[TCP].flags == SYN):
+            p.show()
+            add_mapping(p)
+
